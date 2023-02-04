f3547682fa1f6fe62a008c1cb4647058be9d2709
Ogurinka Tammy Benjamin <ogurinkabenjamin@gmail.com>
2023-02-04 12:27:58 +0100

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 // Add cors
 const cors = require("cors")({ origin: true });
+app.use(cors);
 // Import Body Parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
